7c9b813b10ce45de7826349baa1b6b951f3cd9b6
K4NEI <kimyoungho0415@gmail.com>
2026-05-11 12:25:31 +0900

    .

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -17,10 +17,10 @@ dependencies {
 
     runtimeOnly("com.h2database:h2")
 
-    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    api("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
-    kapt("jakarta.persistence:jakarta.persistence-api")
-    kapt("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.persistence:jakarta.persistence-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
 }
 
 sourceSets {
--- order/build.gradle.kts
+++ order/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 dependencies {
-    implementation("com.github.kanei0415:ktcloud-msa-common:v1.0.2")
+    implementation("com.github.kanei0415:ktcloud-msa-common:v1.0.3")
 
     implementation("org.springframework.boot:spring-boot-starter-web")
 
@@ -19,8 +19,8 @@ dependencies {
 
     implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
     kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
-    kapt("jakarta.persistence:jakarta.persistence-api")
-    kapt("jakarta.annotation:jakarta.annotation-api")
+    implementation("jakarta.persistence:jakarta.persistence-api")
+    implementation("jakarta.annotation:jakarta.annotation-api")
 }
 
 sourceSets {
