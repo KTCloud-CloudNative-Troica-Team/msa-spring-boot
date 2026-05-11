9242cd53e9bb7dceed0d9de65cf62d949722a9e5
K4NEI <kimyoungho0415@gmail.com>
2026-05-11 14:27:16 +0900

    .

--- identification/build.gradle.kts
+++ identification/build.gradle.kts
@@ -12,9 +12,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
     runtimeOnly("com.h2database:h2")
-
-    implementation("com.querydsl:querydsl-jpa:5.1.0:jakarta")
-    kapt("com.querydsl:querydsl-apt:5.1.0:jakarta")
 }
 
 sourceSets {
